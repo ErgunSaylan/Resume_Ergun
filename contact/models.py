1c6246c044957ad0996b8069b51d4f1a56262448
ErgunSaylan <ergunsaylan2117@gmail.com>
2025-05-04 15:03:39 +0300

    feat: pdf cv part connected database

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from core.models import AbstractModel
 # Create your models here.
+
+class Message(AbstractModel):
+    name = models.CharField(
+        default='',
+        max_length=100,
+        blank=True,
+        verbose_name='Name',
+    )
+    email = models.EmailField(
+        default='',
+        verbose_name='Email',
+        blank=True,
+        max_length=254,
+    )
+    subject = models.CharField(
+        default='',
+        max_length=254,
+        blank=True,
+        verbose_name='Subject',
+    )
+    message = models.TextField(
+        default='',
+        verbose_name='Message',
+        blank=True,
+    )
+    def __str__(self):
+        return f'Message:{self.name}'
+
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+        ordering = ('name',)
--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,29 @@
 from django.contrib.messages import success
 from django.http import JsonResponse
 from django.shortcuts import render
+from contact.models import Message
 # Create your views here.
 
 def contact_form(request):
-    context = {'success': True, 'message': 'Contact from send sucessfully.'}
+    if request.POST:
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        Message.objects.create(
+             name=name,
+             email=email,
+             subject=subject,
+             message=message)
+
+        succes =True
+        message = 'Contact form sent successfully'
+    else:
+        succes = False
+        message = 'Request method is not valid'
+
+    context = {'success': succes, 'message': message}
     return JsonResponse(context)
 def contact(request):
     return render(request, 'contact.html')
